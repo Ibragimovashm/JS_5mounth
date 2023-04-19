3e9795855282969fa569af242517270685681af0
Ibragimovashm <119935183+Ibragimovashm@users.noreply.github.com>
2023-04-19 15:39:22 +0600

    Add files via upload

--- DZ1_Yana_22-03/src/pages/AboutPage/AboutPage.jsx
+++ DZ1_Yana_22-03/src/pages/AboutPage/AboutPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 
 const AboutPage = () => {
   return <div>AboutPage</div>;
--- DZ1_Yana_22-03/src/pages/PokemonPage/PokemonPage.jsx
+++ DZ1_Yana_22-03/src/pages/PokemonPage/PokemonPage.jsx
@@ -1,24 +1,19 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
+import EveryPokemon from "../../components/EveryPokemon/EveryPokemon";
 
 const PokemonPage = () => {
   const [pokemon, setPokemon] = useState([]);
   useEffect(() => {
     axios
       .get("https://pokeapi.co/api/v2/pokemon")
-      .then((resp) => setPokemon(resp.data.results));
+      .then((response) => setPokemon(response.data.results));
   }, []);
-  console.log(pokemon);
   return (
     <div>
-      <ul>
-        {pokemon.map((poki, index) => (
-          <li key={index}>
-            <NavLink to={`${index + 1}`}>{poki.name}</NavLink>
-          </li>
-        ))}
-      </ul>
+      {pokemon.map((poki, index) => (
+        <EveryPokemon poki={poki} key={index} />
+      ))}
     </div>
   );
 };
